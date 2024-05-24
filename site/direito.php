9779a8fb6d091aad9e3f7d77122dc9654f451b31
ThiagoR <thiago.ralmeida@a.ficr.edu.br>
2024-05-24 11:01:27 -0300

    Adicionando o include header

--- site/direito.php
+++ site/direito.php
@@ -1,41 +1,6 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>FICR</title>
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-</head>
-<body>
-	<header>
-		<div class="container">
-			<h1>
-				<a href="index.php">
-					<img src="img/logo.svg" alt="Faculdade Imaculada Conceição do Recife - FICR">
-				</a>
-			</h1>
-			<nav>
-				<input type="checkbox" id="menu-toggle">
-				<label for="menu-toggle" class="menu-icon"></label>
-				<ul class="principal">
-					<li class="emLinha"><a href="index.html">Início</a></li>
-					<li class="emLinha">
-						<a href="#">Cursos <span>▼</span></a>
-						<ul class="oculto">
-							<li><a href="ads.php">Análise e Desenvolvimento de Sistemas</a></li>
-							<li><a href="si.php">Sistemas para Internet</a></li>
-							<li><a href="direito.php">Direito</a></li>
-							<li><a href="enfermagem.php">Enfermagem</a></li>
-							<li><a href="psicologia.php">Psicologia</a></li>
-						</ul>
-					</li>
-					<li class="emLinha"><a href="sobre.php">Sobre nós</a></li>
-					<li class="emLinha"><a href="contato.php">Contato</a></li>
-				</ul>
-			</nav>
-		</div>
-	</header>
-	<main>
+<?php include"header.php"; ?>
+
+<main>
 		<section class="container margemSecao2 interna">
 			<h3>Direito</h3>
 			<div>
--- site/index.php
+++ site/index.php
@@ -1,40 +1,4 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>FICR</title>
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-</head>
-<body>
-	<header>
-		<div class="container">
-			<h1>
-				<a href="index.php">
-					<img src="img/logo.svg" alt="Faculdade Imaculada Conceição do Recife - FICR">
-				</a>
-			</h1>
-			<nav>
-				<input type="checkbox" id="menu-toggle">
-				<label for="menu-toggle" class="menu-icon"></label>
-				<ul class="principal">
-					<li class="emLinha"><a href="index.php">Início</a></li>
-					<li class="emLinha">
-						<a href="#">Cursos <span>▼</span></a>
-						<ul class="oculto">
-							<li><a href="ads.php">Análise e Desenvolvimento de Sistemas</a></li>
-							<li><a href="si.php">Sistemas para Internet</a></li>
-							<li><a href="direito.php">Direito</a></li>
-							<li><a href="enfermagem.php">Enfermagem</a></li>
-							<li><a href="psicologia.php">Psicologia</a></li>
-						</ul>
-					</li>
-					<li class="emLinha"><a href="sobre.php">Sobre nós</a></li>
-					<li class="emLinha"><a href="contato.php">Contato</a></li>
-				</ul>
-			</nav>
-		</div>
-	</header>
+<?php include"header.php"; ?>
 	<main>
 		<section class="container">
 			<div class="blocoFig">
