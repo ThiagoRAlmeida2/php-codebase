92b7f9e4fc0439cb22ce1975c35f94405ac1753c
ThiagoR <thiago.ralmeida@a.ficr.edu.br>
2024-05-24 10:12:58 -0300

    Initial commit

---
+++ aula1.php
@@ -0,0 +1,22 @@
+<?php
+    $nome =  "Thiago";
+    $sobrenome = " Ribeiro";
+    $nomeCompleto = $nome . " " .$sobrenome;
+?>
+
+<!DOCTYPE html>
+<html lang="pt">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,
+            initial-scale=1">
+        <title> Aula 1</title>
+    </head>
+<body>
+    <h1> Aula 1</h1>
+    <?php
+
+
+    ?>
+</body>
+</html>
